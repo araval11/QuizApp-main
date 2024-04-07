a2bf4ad2e0fc0f4be92a46aa6311a08e7972e99e
AakashRaval <ravalaakash11@gmail.com>
2024-04-07 00:46:16 -0400

    Initial Commit

---
+++ QuizApp/Question.swift
@@ -0,0 +1,9 @@
+
+
+import Foundation
+
+struct Question {
+    let question: String
+    let correctAnswer: String
+    let incorrectAnswers: [String]
+}
---
+++ QuizApp/ScorePageViewController.swift
@@ -0,0 +1,17 @@
+
+
+import UIKit
+
+class ScorePageViewController: UIViewController {
+
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var score: String?  
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            if let score = score {
+                scoreLabel.text = "Score: \(score)"
+            }
+        }
+    }
